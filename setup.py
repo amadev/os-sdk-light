eda7267da5243cc77ec811e8c18bf3f9d7606142
Andrey Volkov <avolkov@mirantis.com>
2018-07-18 14:59:57 +0300

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 setup(
     name="os-sdk-light",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     # metadata for upload to PyPI
     author="Andrey Volkov",
