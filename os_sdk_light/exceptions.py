3ea41b72cbf3ad01337b0bf22eb60e97c574b9cf
Andrey Volkov <avolkov@mirantis.com>
2018-07-18 14:09:04 +0300

    Exception docstrings

--- os_sdk_light/exceptions.py
+++ os_sdk_light/exceptions.py
@@ -3,12 +3,35 @@ class BaseException(Exception):
 
 
 class CannotConnectToCloud(BaseException):
+
+    """Error while gettting endpoint for service.
+
+    Errors like invalid connection configuration, invalid auth,
+    no endpoint exists, etc.
+
+    """
+
     pass
 
 
 class SchemaError(BaseException):
+
+    """Schema reading related error.
+
+    Errors like no schema file, invalid format, etc.
+
+    """
+
     pass
 
 
 class ValidationError(BaseException):
+
+    """Error when input/output does not match correcpoding schema.
+
+    Both request and response could have json schemas, if they does not match
+    actual data the exception occured.
+
+    """
+
     pass
